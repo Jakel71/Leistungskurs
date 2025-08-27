b5ea18b92417836a5e7a66061ec2e43f69428d06
Jakel71 <ja@klupsch.de>
2025-08-27 16:40:01 +0200

    AB11--> Nr.8

---
+++ Schule/src/Blatt01/TypKonversion.java
@@ -0,0 +1,35 @@
+package Blatt01;
+
+public class TypKonversion {
+    public static void main(String[] args) {
+        int a = 14;
+        double b = -21.25;
+
+        int c = (int) b; // konvertierung double zu int
+        double d = (double) a; // Konvertierung int zu double
+
+        System.out.println(c);
+        System.out.println(d);
+
+        // Zusammenhang zwischen Int und double: bei int wird alles hinter dem Komma ignoriert, bei Rückkonvertierung wird ".0" angehängt.
+
+        char e = 'a';
+        char f = 65;
+        int g = (int) e;
+        char h = (char) f;
+        System.out.println(g);
+        System.out.println(h);
+
+        //Zusammenhang zwischen int und char: Konvertierung mit ASCII
+
+        //Ausgabe ist die Konvertierte Form, int <--> double und char <--> int
+
+        // e)
+        //boolean t = true;
+        //h= (char) t;
+        // erzeugt Fehler, da man Wahr nicht in einen Buchstaben bzw. Zeichen umwandeln kann!!!
+
+        // Frage an Herr Schisch: Warum wird Warnung wegen redundanz bei konvertierung angezeit?
+
+    }
+}
