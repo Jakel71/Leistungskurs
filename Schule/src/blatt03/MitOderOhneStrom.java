079705291fb139586b9106de17d993a3321f12d7
Jakel71 <ja@klupsch.de>
2025-09-04 10:43:30 +0200

    AB 17 --> Nr.7

--- Schule/src/blatt03/MitOderOhneStrom.java
+++ Schule/src/blatt03/MitOderOhneStrom.java
@@ -8,10 +8,13 @@ public class MitOderOhneStrom {
         // Auswählen des Gatters
         System.out.println("Wählen sie bitte ein Gatter: NOT; OR; AND; XOR");
         String gatter = input.nextLine();
+        gatter = gatter.toUpperCase(); //Groß und kleinschreibungsfehler abfangen
         if (gatter.equals("NOT")) {
+            // Example mäßige Erklärung für alle Cases:
+            // Erklärung, dann eingabe der 1-2 Werte (true / false)
             System.out.println("Das NOT-Gatter:\nDas Gatter verfügt über einen Eingang und einen Ausgang.\nBefindet sich auf dem ersten Eingang Strom? (true/false)");
             Boolean strom1 = input.nextBoolean();
-            System.out.printf("%b NOT = %b", strom1, !strom1);
+            System.out.printf("%b NOT = %b", strom1, !strom1); // Verarbeitung und Ausgabe
         }else if (gatter.equals("AND")) {
             System.out.println("Das AND-Gatter:\nDas Gatter verfügt über 2 Eingänge und einen Ausgang.\nBefindet sich auf dem ersten Eingang Strom? (true/false)");
             Boolean strom1 = input.nextBoolean();
@@ -19,10 +22,18 @@ public class MitOderOhneStrom {
             Boolean strom2 = input.nextBoolean();
             System.out.printf("%b AND %b = %b", strom1, strom2, strom1 == strom2);
         }else if (gatter.equals("XOR")) {
-            System.out.println("Das XOR-Gatter");
+            System.out.println("Das XOR-Gatter:\nDas Gatter verfügt über 2 Eingänge und einen Ausgang.\nBefindet sich auf dem ersten Eingang Strom? (true/false)");
+            Boolean strom1 = input.nextBoolean();
+            System.out.println("Befindet sich auf dem zweiten Eingang Strom? (true/false)");
+            Boolean strom2 = input.nextBoolean();
+            System.out.printf("%b XOR %b = %b", strom1, strom2, strom1 != strom2);
         }else if (gatter.equals("OR")) {
-
-        }else{
+            System.out.println("Das OR-Gatter:\nDas Gatter verfügt über 2 Eingänge und einen Ausgang.\nBefindet sich auf dem ersten Eingang Strom? (true/false)");
+            Boolean strom1 = input.nextBoolean();
+            System.out.println("Befindet sich auf dem zweiten Eingang Strom? (true/false)");
+            Boolean strom2 = input.nextBoolean();
+            System.out.printf("%b OR %b = %b", strom1, strom2, strom1 != strom2 || strom1 && strom2);
+        }else{ // Alle falschen eingaben werden benannt und das Programm wird beendet
             System.out.println("Sie müssen ein Gatter eingeben, dass Programm beendet sich jetzt.");
             System.exit(0);
         }
