3df46d5be61d5f5835f171802a452d5f99ae3cbc
Jakel71 <ja@klupsch.de>
2025-09-08 14:22:19 +0200

    ETF.java

---
+++ Schule/src/blatt04/ETF.java
@@ -0,0 +1,30 @@
+package blatt04;
+
+import java.util.Scanner;
+
+public class ETF {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double wert = 3578.89;
+        double wertsteigerung = 6.00;
+        System.out.println("Welchen betrag möchten sie im Monat anlegen?");
+        double sparBetrag = input.nextDouble();
+        System.out.println("Wie lange möchten sie Anlegen? (in Monaten)");
+        int laufzeit = input.nextInt();
+        double anteil = 0;
+        double gewinn = 0;
+        int i = 0;
+        double totalSparBetrag = 0;
+        while(i<laufzeit) {
+            i++;
+            totalSparBetrag += sparBetrag;
+            anteil = totalSparBetrag/wert;
+            System.out.println("Monat:\t\t\t\t\t\t\t" + i);
+            System.out.println("Gesamter Sparbetrag:\t\t\t" + totalSparBetrag);
+            System.out.println("Gesamter Anteil an der Aktie:\t" + anteil);
+        }
+        gewinn += (anteil*wert)*wertsteigerung/100;
+        System.out.printf("Gewinn:\t\t\t\t\t\t\t%.2f€",gewinn);
+
+    }
+}
