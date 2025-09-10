2e3b847038809846fdff24233fe2c2342c42e8f4
JanKlupsch <ja@klupsch.de>
2025-09-10 11:16:02 +0200

    Schlag die Kuh: input und steuerung, noch erledigen: Wärmer/kälter und don't exit gatter

---
+++ Schule/src/blatt04/schlagDieKuh.java
@@ -0,0 +1,39 @@
+package blatt04;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class schlagDieKuh {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Random rnd = new Random();
+        int topfX = rnd.nextInt(-5, +5);
+        int topfY = rnd.nextInt(-5, +5);
+        int playerX = rnd.nextInt(-5, +5);
+        int playerY = rnd.nextInt(-5, +5);
+        System.out.printf("Das Spiel beginnt: Sie starten bei %d und %d.",playerX,playerY);
+        double playerDifX = playerX - topfX;
+        double playerDifY = playerY - topfY;
+        while (topfX != playerX && topfY != playerY){
+            double oldPlayerDifX = playerDifX;
+            double oldPlayerDifY = playerDifY;
+            System.out.println("Wohin möchten sie gehen?");
+            char steuerung = input.next().charAt(0);
+            steuerung = Character.toLowerCase(steuerung);
+            switch (steuerung) {
+                case 'w':
+                    playerY += 1;
+                case 's':
+                    playerY -= 1;
+                case 'a':
+                    playerX -= 1;
+                case 'd':
+                    playerX += 1;
+            }
+            playerDifX = playerX - topfX;
+            playerDifY = playerY - topfY;
+
+
+        }
+    }
+}
