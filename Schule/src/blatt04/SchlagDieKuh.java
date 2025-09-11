743ec455c03aa2b509adc6071b3b6a3448340a58
Jakel71 <ja@klupsch.de>
2025-09-11 20:12:55 +0200

    Ab 19 --> Nr.7 + Schlag die Kuh Optimierung

--- Schule/src/blatt04/SchlagDieKuh.java
+++ Schule/src/blatt04/SchlagDieKuh.java
@@ -7,33 +7,90 @@ public class SchlagDieKuh {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Random rnd = new Random();
-        int topfX = rnd.nextInt(-5, +5);
-        int topfY = rnd.nextInt(-5, +5);
-        int playerX = rnd.nextInt(-5, +5);
-        int playerY = rnd.nextInt(-5, +5);
-        System.out.printf("Das Spiel beginnt: Sie starten bei %d und %d.",playerX,playerY);
-        double playerDifX = playerX - topfX;
-        double playerDifY = playerY - topfY;
-        while (topfX != playerX && topfY != playerY){
-            double oldPlayerDifX = playerDifX;
-            double oldPlayerDifY = playerDifY;
-            System.out.println("Wohin möchten sie gehen?");
-            char steuerung = input.next().charAt(0);
-            steuerung = Character.toLowerCase(steuerung);
-            switch (steuerung) {
-                case 'w':
-                    playerY += 1;
-                case 's':
-                    playerY -= 1;
-                case 'a':
-                    playerX -= 1;
-                case 'd':
-                    playerX += 1;
+        System.out.println("Möchten sie Schlag die Kuh spielen (true) oder möchten sie das Programm spielen lassen(false)?");
+        boolean schlagDieKuh = input.nextBoolean();
+        if (schlagDieKuh) {
+            int topfX = rnd.nextInt(0, +10);
+            int topfY = rnd.nextInt(0, +10);
+            int playerX = rnd.nextInt(0, +10);
+            int playerY = rnd.nextInt(0, +10);
+            System.out.printf("Das Spiel beginnt: Sie starten bei %d und %d.\n", playerX, playerY);
+            double playerDifX = topfX - playerX;
+            double playerDifY = topfY - playerY;
+            int i = 0;
+            while (topfX != playerX && topfY != playerY) {
+                i++;
+                double oldPlayerDifX = playerDifX;
+                double oldPlayerDifY = playerDifY;
+                System.out.println("Wohin möchten sie gehen?");
+                char steuerung = input.next().charAt(0);
+                steuerung = Character.toLowerCase(steuerung);
+                switch (steuerung) {
+                    default:
+                        System.out.println("Dies ist kein zugelassener Befehl. Zugelassene befehle sind w / a / s / d");
+                    case 'w':
+                        playerY += 1;
+                    case 's':
+                        playerY -= 1;
+                    case 'a':
+                        playerX -= 1;
+                    case 'd':
+                        playerX += 1;
+                }
+                playerDifX = playerX - topfX;
+                playerDifY = playerY - topfY;
+
+                if (playerDifX < oldPlayerDifX || playerDifY < oldPlayerDifY) {
+                    System.out.println("Wärmer");
+                } else if (playerDifX > oldPlayerDifX || playerDifY > oldPlayerDifY) {
+                    System.out.println("Kälter");
+                } else {
+                    System.out.println("Gleichwarm?!");
+                }
+
+
             }
-            playerDifX = playerX - topfX;
-            playerDifY = playerY - topfY;
+            System.out.println("Sie haben gewonnen. Sie haben " + i + " Züge gebraucht.");
+        } else {
+            int i = 0;
+            System.out.println("Geben sie bitte die Y-Korrdinate des Startpunktes an:");
+            int playerY = input.nextInt();
+            System.out.println("Geben sie bitte die X-Korrdinate des Startpunktes an:");
+            int playerX = input.nextInt();
+            System.out.println("Geben sie bitte die Y-Korrdinate des Endpunktes an:");
+            int topfY = input.nextInt();
+            System.out.println("Geben sie bitte die X-Korrdinate des Endpunktes an:");
+            int topfX = input.nextInt();
+            double playerDifX = topfX-playerX;
+            if (playerDifX > 0) {
 
-
+                while (playerX != topfX) {
+                    playerX += 1;
+                    i++;
+                    System.out.printf("Das Programm geht einen Schritt nach rechts. Neuer Punkt: (%d|%d)\n", playerX, playerY);
+                }
+            } else {
+                while (playerX != topfX) {
+                    playerX -= 1;
+                    i++;
+                    System.out.printf("Das Programm geht einen Schritt nach links. Neuer Punkt: (%d|%d)\n", playerX, playerY);
+                }
+            }
+            double playerDifY = topfY - playerY;
+            if (playerDifY > 0) {
+                while (playerY != topfY) {
+                    playerY += 1;
+                    i++;
+                    System.out.printf("Das Programm geht einen Schritt nach oben. Neuer Punkt: (%d|%d)\n", playerX, playerY);
+                }
+            } else {
+                while (playerY != topfY) {
+                    playerY -= 1;
+                    i++;
+                    System.out.printf("Das Programm geht einen Schritt nach unten. Neuer Punkt: (%d|%d)\n", playerX, playerY);
+                }
+            }
+            System.out.println("Das Programm erreicht den Endpunkt mit " + i + " Schritten.");
         }
     }
 }
