28306187822c29f2b77ee41a094999bdaea05972
JanKlupsch <ja@klupsch.de>
2025-09-15 09:31:26 +0200

    Bowling anfang

---
+++ Schule/src/blatt05/Bolwling.java
@@ -0,0 +1,11 @@
+package blatt05;
+
+import java.util.Scanner;
+
+public class Bolwling {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int counter = 0;
+        int multiplier = 1;
+    }
+}
