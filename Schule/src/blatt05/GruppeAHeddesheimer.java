94afb9a1236c95560cc1b74180bff4c8a0f0b5f8
JanKlupsch <ja@klupsch.de>
2025-09-17 11:17:54 +0200

    17.9.2025-Stunde Heddesheimer

---
+++ Schule/src/blatt05/GruppeAHeddesheimer.java
@@ -0,0 +1,19 @@
+package blatt05;
+
+import java.util.Scanner;
+
+public class GruppeAHeddesheimer {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Einlesen der Punkten, max und erreichte
+        System.out.print("Geben Sie die Anzahl der Noten ein: ");
+        int anzahl = input.nextInt();
+        int erfahrung = anzahl;
+        int notenwerte =0;
+        while (erfahrung >0) {
+            System.out.print("Bitte geben Sie ihre Note ein: ");
+            int note = input.nextInt();
+
+        }
+    }
+}
