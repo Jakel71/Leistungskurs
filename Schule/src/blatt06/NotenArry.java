f5e373a7f3b1a52ab9ae3f14c8a71411f668f45b
Jakel71 <ja@klupsch.de>
2025-09-22 11:40:43 +0200

    NotenArray fertiggestellt

--- Schule/src/blatt06/NotenArry.java
+++ Schule/src/blatt06/NotenArry.java
@@ -7,21 +7,29 @@ public class NotenArry {
         Scanner input = new Scanner(System.in);
         System.out.print("Wie viele Schüler gibt es in Ihrer Klasse? ");
         int anzSchueler =  input.nextInt();
-        int [] noten = new int[anzSchueler];
+        int [] noten = new int[anzSchueler]; // Array initialisieren
+        double durchschnitt = 0;
         for (int i = 0; i<anzSchueler; i++) {
-            do {
-                System.out.println("Welche Note hat ihr Schueler " + i + "?");
-                noten[i] = input.nextInt();
-            }   while (0 > noten[i] && noten[i] > 7);
+            System.out.println("Welche Note hat Ihr Schueler " + (i+1) + "?");
+            noten[i] = input.nextInt(); // Wert setzten für Array für Element i
+            if (noten[i] <0 || noten[i]>7){
+                noten[i]=-1;
+                i--;
+                continue;
+            }
+
         }
 
         for (int i = 0 ; i < noten.length ; i++) {
-            System.out.println("Note von Schüler " + (i+1) + " hat die Note: " + noten[i]);
+            System.out.println("Note von Schüler " + (i+1) + " hat die Note: " + noten[i]); // Zugriff auf Array, Element i
         }
         System.out.println("\nReverse order:");
 
         for (int i = noten.length -1 ; i >= 0 ; i --) {
             System.out.println("Note von Schüler " + (i+1) + " hat die Note: " + noten[i]);
+            durchschnitt += noten[i];
         }
+        durchschnitt/=noten.length;
+        System.out.printf("\n Der Durchschnitt der Noten beträgt: %.2f", durchschnitt);
     }
 }
