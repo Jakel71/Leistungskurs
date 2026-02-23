8a27bf48c7da87dd41544141914b24ef4a808bc0
Jakel71 <ja@klupsch.de>
2026-02-23 18:11:03 +0100

    Gitinore - hope 2.0

---
+++ Schule/src/blatt16/Labyrinth.java
@@ -0,0 +1,8 @@
+package blatt16;
+
+public class Labyrinth {
+    public static char[][] leseLabyrinth(int numLaby){
+
+    }
+    public static void main(String[] args){}
+}
--- Schule/src/blatt16/Suchkommando.java
+++ Schule/src/blatt16/Suchkommando.java
@@ -8,6 +8,13 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Suchkommando {
+
+    /**
+     * sucht eine Datei in einem Ordner und dessen Unterordner
+     * @param dateiName Name der Datei + Endung (z.B. ".txt")
+     * @param dir Ordner, kann auch "C:" sein
+     * @return String Liste mit allen gefundenen Elementen
+     */
     public static String[] suchen(String dateiName, String dir){
         Scanner input = new Scanner( System.in );
         ArrayList<String> found = new ArrayList<String>();
@@ -16,26 +23,36 @@ public class Suchkommando {
             System.out.println("Startverzeichnis existiert nicht oder ist kein Verzeichnis!");
             return null;
         }
-        ArrayList<String> liste = new ArrayList<String>();
-        String[] files = root.list();
+        ArrayList<File> liste = new ArrayList<>();
+        File[] files = root.listFiles();
         for(int i=0;i<files.length;i++){
             liste.add(files[i]);
         }
 
-//        ArbeitMitArrays.printArray(files);
+        while(!liste.isEmpty()) {
 
-        for(String s : liste){
-            File f =  new File(root,s);
-            if(s.equals(dateiName)){
-                System.out.println(f.getAbsolutePath());
-                found.add(f.getAbsolutePath());
-                System.out.println("Soll weiter gesucht werden? (Y/N): ");
-                if(input.next() == "N"){
-                    return found.toArray(new String[0]);
+                File f = liste.get(0);
+
+                if (f.getName().equals(dateiName)) {
+                    System.out.println(f.getAbsolutePath());
+                    found.add(f.getAbsolutePath());
+
+                    System.out.println("Soll weiter gesucht werden? (Y/N): ");
+                    if (input.next().toUpperCase() == "N") {
+                        return found.toArray(new String[0]);
+                    }
+                } else if (f.isDirectory()) {
+                    File[] newFiles = f.listFiles();
+                    if(newFiles != null) {
+                        for (int j = 0; j < newFiles.length; j++) {
+                            liste.add(newFiles[j]);
+//                            System.out.println(newFiles[j].getAbsolutePath());
+                        }
+                    }
                 }
-            }
+                liste.remove(0);
         }
-        return liste.toArray(new String[0]);
+        return found.toArray(new String[0]);
     }
 
     public static void main(String[] args) {
@@ -43,7 +60,7 @@ public class Suchkommando {
         for(File f : roots){
             System.out.println(f.getAbsolutePath());
         }
-        String[] found = suchen("1.txt","Schule" + File.separator + "src" + File.separator + "blatt16" +  File.separator + "sprengtest");
+        String[] found = suchen("1.txt",roots[0].getAbsolutePath());//"Schule" + File.separator + "src" + File.separator + "blatt16" +  File.separator + "sprengtest");
         ArbeitMitArrays.printArray(found);
     }
 }
